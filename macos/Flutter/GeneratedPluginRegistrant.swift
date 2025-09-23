9531c4ad558efd74fe0e898bd297a0c3824da4e1
NolenM93 <nolenmillington9@gmail.com>
2025-09-23 14:48:20 -0400

    Add cross-platform support files and Firebase configuration

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< Updated upstream
 import cloud_firestore
 import firebase_analytics
 import firebase_auth
 import firebase_core
+=======
+>>>>>>> Stashed changes
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -16,10 +19,13 @@ import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+<<<<<<< Updated upstream
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+=======
+>>>>>>> Stashed changes
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
