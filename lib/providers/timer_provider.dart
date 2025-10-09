867b3294fbb6e62fc3af5485ea8a6c7b019dc4f7
Yasmani Acosta <Yacosta@student.fullsail.edu>
2025-10-09 14:55:32 -0400

    refactor: Rename private method to public for JSON parsing in CalendarEvent model

--- lib/providers/timer_provider.dart
+++ lib/providers/timer_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart'; // Not needed, included in material.dart
 import 'package:flutter/material.dart';
 import 'dart:async';
 
